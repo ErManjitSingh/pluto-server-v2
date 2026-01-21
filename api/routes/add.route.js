763b8b4b3343792ee21665872db63172d07b3e05
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-01-21 12:40:23 +0530

    Update add.route.js

--- api/routes/add.route.js
+++ api/routes/add.route.js
@@ -4,6 +4,7 @@ import {
   getAdds, 
   getAdd, 
   updateAdd, 
+  updateAddMediaAndCanonical,
   deleteAdd,
   deleteMultipleAdds,
   getPackageOnly 
@@ -16,6 +17,7 @@ router.get('/get', getAdds);
 router.get('/get/:id', getAdd);
 router.get('/packages', getPackageOnly);
 router.put('/update/:id', updateAdd);
+router.put('/update-media/:id', updateAddMediaAndCanonical);
 router.delete('/delete/:id', deleteAdd);
 router.delete('/delete-multiple', deleteMultipleAdds);
 
