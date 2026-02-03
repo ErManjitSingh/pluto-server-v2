812ef34d06fe9e24f44c7773828334731103cbb2
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-02-03 10:42:36 +0530

    Create customer.model.js

---
+++ api/models/customer.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    hotel: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+    cab: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+    activites: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+    other: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+    response: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+    leadata: {
+      type: mongoose.Schema.Types.Mixed,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("CustomerData", customerSchema);
