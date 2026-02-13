1ec9f4d32fdeb10ab0d31f0c0bc563c826894237
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-02-13 17:06:58 +0530

    Update hotelbooking.route.js

--- api/routes/hotelbooking.route.js
+++ api/routes/hotelbooking.route.js
@@ -3,6 +3,8 @@ import {
   getAllHotelBookings,
   createHotelBooking,
   getHotelBookingById,
+  getHotelBookingsByPropertyName,
+  updateBookingResponse,
   updateHotelBooking,
   deleteHotelBooking,
 } from '../controllers/hotelbooking.controller.js';
@@ -10,9 +12,11 @@ import {
 const router = express.Router();
 
 router.get('/get', getAllHotelBookings);
+router.get('/getbypropertyname', getHotelBookingsByPropertyName);
 router.post('/create', createHotelBooking);
 router.get('/:id', getHotelBookingById);
 router.put('/update/:id', updateHotelBooking);
+router.patch('/:id/bookingresponse', updateBookingResponse);
 router.delete('/delete/:id', deleteHotelBooking);
 
 export default router;
