0a4e33aafb9a6041261b456a5286f20915f82301
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-02-13 16:31:59 +0530

    Create hotelbooking.model.js

---
+++ api/models/hotelbooking.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const hotelBookingSchema = new mongoose.Schema(
+  {
+    bookingId: {
+      type: String,
+    },
+    cityName: {
+      type: String,
+    },
+    contactInfo: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    daysWithDates: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    hotels: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    numberOfGuests: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    numberOfRooms: {
+      type: String,
+    },
+    propertyName: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    totalAmount: {
+      type: Number,
+    },
+    bookingresponse: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+  },
+  { timestamps: true }
+);
+
+const HotelBooking = mongoose.model('HotelBooking', hotelBookingSchema);
+export default HotelBooking;
