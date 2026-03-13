243f5d9f50ee58669e385f2825c8e9fda6edfc17
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-03-13 13:26:41 +0530

    Create googleCalendar.route.js

---
+++ api/routes/googleCalendar.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { connectGoogleCalendar, googleCalendarCallback } from '../controllers/googleCalendar.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+// Executive connects their Google Calendar
+router.get('/connect', verifyToken, connectGoogleCalendar);
+
+// Google OAuth callback (public; validates state internally)
+router.get('/callback', googleCalendarCallback);
+
+export default router;
+
