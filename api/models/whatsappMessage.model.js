371a9ef41869523dc6ea12a94b93f666f9a1a754
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-04-21 16:37:45 +0530

    Update whatsappMessage.model.js

--- api/models/whatsappMessage.model.js
+++ api/models/whatsappMessage.model.js
@@ -17,7 +17,6 @@ const whatsappMessageSchema = new mongoose.Schema(
     filename: { type: String, default: null },
     metaMediaId: { type: String, default: null },
     mimeType: { type: String, default: null },
-    status: { type: String, enum: ['sent', 'delivered', 'read', 'failed'], default: null },
   },
   { timestamps: true }
 );
