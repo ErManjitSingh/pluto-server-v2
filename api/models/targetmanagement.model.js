6effc8ea16d8e2348de3e2983b134f83c1d54b2f
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-05-05 15:55:09 +0530

    Update targetmanagement.model.js

--- api/models/targetmanagement.model.js
+++ api/models/targetmanagement.model.js
@@ -9,30 +9,34 @@ const targetManagementSchema = new mongoose.Schema(
       lowercase: true,
       match: [/^[a-z]{3,9}\d{4}$/, 'Month must be like may2026'],
     },
-    targetData: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    numberOfLeads: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    userId: {
-      type: mongoose.Schema.Types.Mixed,
-      required: true,
-    },
-    teamLeaderId: {
-      type: mongoose.Schema.Types.Mixed,
-      required: false,
-      default: null,
-    },
-    managerId: {
-      type: mongoose.Schema.Types.Mixed,
-      required: false,
-      default: null,
-    },
+    targets: [
+      {
+        targetData: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        numberOfLeads: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        userId: {
+          type: mongoose.Schema.Types.Mixed,
+          required: true,
+        },
+        teamLeaderId: {
+          type: mongoose.Schema.Types.Mixed,
+          required: false,
+          default: null,
+        },
+        managerId: {
+          type: mongoose.Schema.Types.Mixed,
+          required: false,
+          default: null,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
