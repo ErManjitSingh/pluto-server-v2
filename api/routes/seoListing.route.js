9b6de4aadce5cb1aee568458bb49a86b89c95886
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-06-20 13:44:58 +0530

    Update seoListing.route.js

--- api/routes/seoListing.route.js
+++ api/routes/seoListing.route.js
@@ -19,7 +19,7 @@ router.get('/sitemap', getSeoListingsForSitemap);
 router.get('/id/:id', getSeoListingById);
 router.get('/slug/:slug', getSeoListingBySlug);
 router.get('/type/:locationType', getSeoListingsByLocationType);
-router.get('/:category/:locationType', getSeoListingsByCategoryAndLocationType);
+router.get('/:category/:locationType/:stateOrCity', getSeoListingsByCategoryAndLocationType);
 router.put('/update/:id', updateSeoListing);
 router.delete('/delete/:id', deleteSeoListing);
 
