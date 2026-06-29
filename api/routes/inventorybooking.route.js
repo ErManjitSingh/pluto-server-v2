7f5483bf1603a8922cee8fb9fda69634e6b2acff
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-06-29 15:06:45 +0530

    Update inventorybooking.route.js

--- api/routes/inventorybooking.route.js
+++ api/routes/inventorybooking.route.js
@@ -6,6 +6,7 @@ import {
   getInventoryBookingsByMobile,
   updateInventoryBooking,
   deleteInventoryBooking,
+  deleteAllInventoryBookings,
   guestLogin,
 } from '../controllers/inventorybooking.controller.js';
 
@@ -18,5 +19,6 @@ router.get('/get-by-mobile/:mobile', getInventoryBookingsByMobile);
 router.get('/get/:id', getInventoryBookingById);
 router.put('/update/:id', updateInventoryBooking);
 router.delete('/delete/:id', deleteInventoryBooking);
+router.delete('/delete-all', deleteAllInventoryBookings);
 
 export default router;
