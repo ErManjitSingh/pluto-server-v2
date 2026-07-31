79fcbb9a568d53d16f1f75605b288b7defc7707b
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-07-31 16:13:23 +0530

    Update margin.model.js

--- api/models/margin.model.js
+++ api/models/margin.model.js
@@ -1,16 +1,5 @@
 import mongoose from 'mongoose';
-const editDiscountSchema = new mongoose.Schema({
-    packageId: { type: String, required: true },
-    packageName: { type: String, required: true },
-    package:{
-        type:mongoose.Schema.Types.Mixed,
-        required:true
-    },
-    loginUserDetail: { type: mongoose.Schema.Types.Mixed, required: true },
-    discountPercentage: { type: Number, required: true },
-    accept: { type: String }, // or Boolean, as per your use
-    managerName: { type: String, required: true }
-}, { _id: true }); // _id is true by default, but explicit for clarity
+
 const marginSchema = new mongoose.Schema({
     state: {
         type: String,
@@ -27,8 +16,7 @@ const marginSchema = new mongoose.Schema({
         lessThan1Lakh: { type: String, required: true },
         between1To2Lakh: { type: String, required: true },
         between2To3Lakh: { type: String, required: true },
-        moreThan3Lakh: { type: String, required: true },
-        editDiscount: [editDiscountSchema]
+        moreThan3Lakh: { type: String, required: true }
     }
 }, { timestamps: true });
 
