65776b5eabd8beca91bb6679ed4a03f62d12c6d7
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-08-15 15:14:38 +0530

    Update razorpayDemand.route.js

--- api/routes/razorpayDemand.route.js
+++ api/routes/razorpayDemand.route.js
@@ -3,12 +3,14 @@ import {
   createDemandOrder,
   verifyDemandPayment,
   getDemandPayment,
+  getAllDemandPayments,
   getDemandRazorpayKey,
 } from '../controllers/razorpayDemand.controller.js';
 
 const router = express.Router();
 
 router.get('/key', getDemandRazorpayKey);
+router.get('/payments', getAllDemandPayments);
 router.post('/order', createDemandOrder);
 router.post('/verify', verifyDemandPayment);
 router.get('/order/:orderId', getDemandPayment);
