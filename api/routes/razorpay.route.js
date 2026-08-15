f0f9b504b056e9ee5aa3b156ec29b7ba7efd87f5
Er Manjit Singh <manjitsingh012345@gmail.com>
2026-08-15 15:13:46 +0530

    Delete api/routes/razorpay.route.js

--- api/routes/razorpay.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-// import { verifyToken } from '../utils/verifyUser.js';
-import { makePayment } from '../controllers/razorpay.controller.js';
-
-const router = express.Router();
-
-router.post('/order', makePayment);
-
-export default router;
